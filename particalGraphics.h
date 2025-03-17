a3f5275ca19973e6234370f03a5f2d62632b4164
Madla <1001348@my.scccd.edu>
2025-03-17 13:43:59 -0700

    added H files

--- particalGraphics.h
+++ particalGraphics.h
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string> 
-#include "particle.h"
+#include "cell.h"
+#include "particalClass.h"
 #include "particalSystem.h"
 #include "/public/colors.h"
 

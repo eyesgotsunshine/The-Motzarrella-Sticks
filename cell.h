8dd64c24bcdff60cda7c4a4d94cfe4ddfe95974f
eyesgotsunshine <mj.beachlife@gmail.com>
2025-03-17 14:58:18 -0700

    Update cell.h

--- cell.h
+++ cell.h
@@ -3,30 +3,42 @@
 #include "particle.h"
 using namespace std;
 
-class Cell {
+class Cell { // holds particles
 	private:
-	int data;
+	int data; // want the type to be Particle
  	Cell *prev = nullptr;
     	Cell *next = nullptr;
 
 	public:
-
-	Cell(p = nullptr, Cell* prev = nullptr, Cell* next = nullptr) : data(p), prev(prev), next(next) {}
+	Cell(int *newData = nullptr, Cell *prev_new = nullptr, Cell *next_new = nullptr) : 
+        data(newData), prev(prev_new), next(next_new) {}
 
 ~Cell() {
 	delete data;
 }
 
-Particle* getData() const { return data; }
+Cell *getData() const { 
+	return data; 
+}
 
-void setData(Particle* p) { data = p; }
+void setData(int *newData) { 
+	data = newData; 
+}
 
-Cell* getNext() const { return next; }
+Cell *getNext() const { 
+	return next; 
+}
 
-void setNext(Cell* n) { next = n; }
+void setNext(Cell *next_new) { 
+	next = next_new; 
+}
 
-Cell* getPrev() const { return prev; }
+Cell *getPrev() const { 
+	return prev; 
+}
 
-void setPrev(Cell* p) { prev = p; }
+void setPrev(Cell *prev_new) { 
+	prev = prev_new; 
+}
 };
 

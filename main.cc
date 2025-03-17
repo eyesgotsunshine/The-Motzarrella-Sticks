b17dccf3505d530f0f9aff6cfab40f9577172776
motsenbocker_0713711 <s.n.motsey2@gmail.com>
2025-03-16 22:24:18 -0700

    Add new file

---
+++ main.cc
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "/public/read.h"
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
+#include <cmath> //IWYU: prgama keep
+#include <stack>
+#include "cell.h"
+#include <string>
+#include "particleSys.h"
+
+using namespace std;
+
+void die() {
+  cout << "BAD INPUT\n";
+//	exit(EXIT_FAILURE);
+}	

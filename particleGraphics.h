07da5d5824089557817d447224d497730e33a3da
Madla <1001348@my.scccd.edu>
2025-03-17 15:54:54 -0700

    commented out main

--- particleGraphics.h
+++ particleGraphics.h
@@ -98,12 +98,12 @@ class ParticleGraphics{
 }
 
 */   
-	void drawRectangle(int x, int y, int width, int hight){
+	void drawRectangle(double x, double y){//(int x, int y, int width, int hight){
 		//std::cout << "Drawing retangle at (" << x << ", " << y << ") with width " << width << " and hight " << hight << " with color " << color << std::endl;// Rectangle Stub
 		return;
 	}
 	
-	void drawOval(int x, int y, int width, int hight){
+	void drawOval(double x, double y){//(int x, int y, int width, int hight){
 		//std::cout << "Drawing oval at (" << x << ", " << y << ") with width " << width << " and hight " << " with color " << color << std::endl;// Oval Stub
 		return;
 	}
@@ -113,12 +113,12 @@ class ParticleGraphics{
 		//std::cout << "Drawing polygon with " << points.size << " points with color  " << color << std::endl;// Polygon Stub
 		return;
 	}
-	void drawLine(int x1, int y1, int x2, int y2) {
+	void drawLine(double x, double y){//(int x1, int y1, int x2, int y2) {
     	//std::cout << "Drawing line from (" << x1 << ", " << y1 << ") to (" << x2 << ", " << y2 << ") with color " << color << std::endl;
     	return;
 	}
 };
-
+/*
 int main() { //test code I wrote on onlineGBD
     ParticleGraphics particleGraphics; //particleGraphics object(black)
 
@@ -128,12 +128,12 @@ int main() { //test code I wrote on onlineGBD
     std::cout << "Updated color: " << particleGraphics.getColor() << std::endl;// Change the color to (red)
 
     particleGraphics.drawPoint(10, 20);
-    particleGraphics.drawRectangle(30, 40, 100, 50);
-    particleGraphics.drawOval(50, 60, 80, 40);
+    particleGraphics.drawRectangle(30, 50);
+    particleGraphics.drawOval(50, 40);
     particleGraphics.drawPolygon(0, 0);
-    //particleGraphics.drawPolygon({{0, 0}, {10, 10}, {20, 5}});
-    particleGraphics.drawLine(0, 0, 10, 10);
+    particleGraphics.drawPolygon(0,0);//({{0, 0}, {10, 10}, {20, 5}});
+    particleGraphics.drawLine(0, 10);
     // Test shapes with the color (red)
 
     return 0;
-}
+}*/

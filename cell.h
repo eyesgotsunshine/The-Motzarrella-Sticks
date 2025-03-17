610d0caa69a6c6a89b8d0c4f70acd822dbe4cd74
motsenbocker_0713711 <s.n.motsey2@gmail.com>
2025-03-17 14:03:00 -0700

    updating

--- cell.h
+++ cell.h
@@ -11,20 +11,22 @@ class Cell {
 
 	public:
 
-	Cell(p = nullptr, Cell* prev = nullptr, Cell* next = nullptr)
-	: data(p), prev(prev), next(next) {}
+	Cell(p = nullptr, Cell* prev = nullptr, Cell* next = nullptr) : data(p), prev(prev), next(next) {}
 
 ~Cell() {
 	delete data;
 }
 
 Particle* getData() const { return data; }
+
 void setData(Particle* p) { data = p; }
 
 Cell* getNext() const { return next; }
+
 void setNext(Cell* n) { next = n; }
 
 Cell* getPrev() const { return prev; }
+
 void setPrev(Cell* p) { prev = p; }
 };
 
